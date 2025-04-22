736a3e60555f614bc12bcd8e02409aad8584aeb7
Soub9977 <soubhagyashirur02@gmail.com>
2025-04-22 20:13:06 +0530

    Add Docker configuration and update API implementation

--- models.py
+++ models.py
@@ -1,6 +1,6 @@
 from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
 from sqlalchemy.orm import relationship
-from moderation.database import Base
+from database import Base
 from datetime import datetime
 
 
